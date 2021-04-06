1cca595c577d7de3802ad83b12e6da7d637b1626
Douglas Morais <douglas.morais@agristamp.com.br>
2021-04-05 21:15:28 -0300

    Auth Decorator  1.0.1

--- agristamp_common/decorators/auth_decorators.py
+++ agristamp_common/decorators/auth_decorators.py
@@ -1,3 +1,4 @@
+import os
 from functools import wraps
 from fastapi import HTTPException
 
@@ -13,15 +14,19 @@ def auth_required_fastapi(func):
         if auth_header[:6] == 'Bearer':
             token = auth_header[7:]
 
-            auth_header = {'Authorization': f'Bearer {token}'}
         else:
             raise HTTPException(500, 'Invalid Authorization header')
         
         # Try authenticate
-        auth_status = service_get('auth_service', 'auth/users/me/', auth_header)
+        auth_service_slug = 'auth_service'
+        auth_endpoint = f'auth/users/services/{os.getenv("SERVICE_SLUG")}'
+        auth_header = {'Authorization': f'Bearer {token}'}
+
+        auth_status = service_get(auth_service_slug, auth_endpoint, auth_header)
         if auth_status.status_code == 200:
+            kwargs['current_user'] = auth_status.json()
             return await func(*args, **kwargs)
         else:
-            raise HTTPException(401, 'Unauthorized')
+            raise HTTPException(auth_status.status_code, auth_status.json()['errors'])
 
     return wrapper
