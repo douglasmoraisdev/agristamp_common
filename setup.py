18d6e302c53ef962bb1f91fc12a113b6502f4c9b
Douglas Morais <msantos.douglas@gmail.com>
2021-11-23 23:18:39 -0300

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="agristamp_common",
-    version="1.5.27",
+    version="1.5.28",
     author="Agristamp",
     author_email="agristamp@agristamp.com.br",
     description="Agristamp Microservices Commom Dependencies",
