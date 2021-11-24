65b2aa2e4f9a3a73e0306085901041b0906c80f6
Douglas Morais <msantos.douglas@gmail.com>
2021-11-23 23:42:52 -0300

    Fix import order

--- agristamp_common/utils/logs.py
+++ agristamp_common/utils/logs.py
@@ -1,12 +1,12 @@
 import logging
 import os
+
+STAGE = os.getenv('STAGE', 'unknow')
+FORMAT = f'[service_log][{STAGE}] %(message)s'
+logging.basicConfig(format=FORMAT)
+
 logger = logging.getLogger()
 
 log_level = os.getenv('LOG_LEVEL') or logging.ERROR
-
-stage = os.getenv('STAGE', 'unknow')
-
-FORMAT = f'[service_log][{stage}] %(message)s'
-logging.basicConfig(format=FORMAT)
 
 logger.setLevel(log_level)
