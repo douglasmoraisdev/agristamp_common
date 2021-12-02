7abb188286eb64a33d1e975ea5231ace7bb0e5d9
Douglas Morais <msantos.douglas@gmail.com>
2021-12-02 12:46:40 -0300

    Adicionado Log level no output do log

--- agristamp_common/utils/logs.py
+++ agristamp_common/utils/logs.py
@@ -13,8 +13,12 @@ logger.setLevel(log_level)
 # create console handler with a higher log level
 ch = logging.StreamHandler()
 ch.setLevel(log_level)
+
+# Level name as String
+str_level = logging.getLevelName(logger.getEffectiveLevel())
+
 # create formatter and add it to the handlers
-formatter = logging.Formatter(f'[service_log][{STAGE}] %(message)s')
+formatter = logging.Formatter(f'[service_log][{STAGE}][{str_level}] %(message)s')
 ch.setFormatter(formatter)
 # add the handlers to logger
 logger.addHandler(ch)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="agristamp_common",
-    version="1.5.32",
+    version="1.5.33",
     author="Agristamp",
     author_email="agristamp@agristamp.com.br",
     description="Agristamp Microservices Commom Dependencies",
