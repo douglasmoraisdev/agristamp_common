af9392dfc0d5819916ea8a05a384b36f1ecf1d5c
Douglas Morais <msantos.douglas@gmail.com>
2022-02-15 14:51:25 -0300

    Validacao de token por nome de client

--- agristamp_common/dependencies/auth_dependency.py
+++ agristamp_common/dependencies/auth_dependency.py
@@ -13,13 +13,13 @@ async def auth_required(
     oauth2_scheme = Depends(oauth2_scheme)
 ):
     if not Authorization:
-        raise HTTPException(HTTP_400_BAD_REQUEST, detail='Invalid Authorization header')        
-    else: 
+        raise HTTPException(HTTP_400_BAD_REQUEST, detail='Invalid Authorization header')
+    else:
 
 
         # Try authenticate internal client
         if os.getenv("AGRISTAMP_KEY") is not None:
-            if Authorization == 'Bearer '+os.getenv("AGRISTAMP_KEY"):                
+            if Authorization == 'Bearer '+os.getenv("AGRISTAMP_KEY"):
                 return {'status': 'ok', 'source': 'agristamp_key'}
 
         # Try authenticate external client
@@ -34,3 +34,38 @@ async def auth_required(
             raise HTTPException(auth_status.status_code, 'Serviço de autenticação indisponível')
         else:
             raise HTTPException(auth_status.status_code, auth_status.json()['errors'])
+
+
+async def token_required(
+    Authorization: str = Header(default=None),
+):
+
+    if not Authorization:
+        raise HTTPException(HTTP_400_BAD_REQUEST, detail='Invalid Authorization header')
+
+    else:
+
+        # Try authenticate
+        authorized_tokens = os.getenv('AUTHORIZED_TOKENS', '').split(',')
+
+        if authorized_tokens is not None:
+
+            for token in authorized_tokens:
+                print(token)
+
+                if Authorization == f'Bearer {token}':
+                    return {'status': 'ok', 'source': 'agristamp_key'}
+
+            # No token found
+            raise HTTPException(HTTP_400_BAD_REQUEST, detail='Invalid Token')
+
+
+def validate_auth_client(auth_token, seguradora_slug):
+
+    # example seguradora-slug:3930293J2093J2093J2093J
+    seguradora_token = auth_token.replace('Bearer ', '').split(':')
+
+    if seguradora_slug == seguradora_token[0]:
+        return True
+    else:
+        raise HTTPException(403, 'Não autorizado')
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="agristamp_common",
-    version="1.6.9",
+    version="1.7.0",
     author="Agristamp",
     author_email="agristamp@agristamp.com.br",
     description="Agristamp Microservices Commom Dependencies",
