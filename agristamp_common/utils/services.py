44aa996a5a1121e69133515ad37e8001bf85cd7f
Douglas Morais <msantos.douglas@gmail.com>
2023-12-08 12:47:20 -0300

    Update services.py
    Adicionado headers.update({'Hash': hash_proposta}) nos service_post e service_get

--- agristamp_common/utils/services.py
+++ agristamp_common/utils/services.py
@@ -386,6 +386,7 @@ def service_get(service_slug, endpoint, headers=None, query=None, force_api_gate
             endpoint = endpoint[1:]
 
         service_url = f"{os.getenv('CLUSTER_URL')}/{os.getenv('STAGE')}/{service_slug}/{endpoint}"
+        headers.update({'Hash': hash_proposta})        
 
         logger.info(f'Enviando GET {service_url} headers:[{headers}] cookies: [{cookies}]',
                       extra={'hash_proposta': hash_proposta, 'data': query})
@@ -414,6 +415,7 @@ def service_post(service_slug, endpoint, headers=None, payload=None, force_api_g
             endpoint = endpoint[1:]
 
         service_url = f"{os.getenv('CLUSTER_URL')}/{os.getenv('STAGE')}/{service_slug}/{endpoint}"
+        headers.update({'Hash': hash_proposta})
 
         logger.info(f'Enviando POST {service_url} headers:[{headers}] cookies: [{cookies}]',
                       extra={'hash_proposta': hash_proposta, 'data': payload})
